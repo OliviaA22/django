0273e0c0379556ef3b9a03465e179a2aa3ed32b7
OliviaA22 <olivia_adaugo@yahoo.com>
2024-10-15 15:51:27 +0200

    admin view modification for polls application

--- my_first_django/my_first_django/settings.py
+++ my_first_django/my_first_django/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'my_first_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
